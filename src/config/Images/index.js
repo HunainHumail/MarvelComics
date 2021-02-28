a82ff31763b4699ff148f5f007d548aa308130db
HunainHumail <hunainhumail786@gmail.com>
2021-02-28 17:21:47 +0500

    Added Application Logos and Backgrounds in image config

--- src/config/Images/index.js
+++ src/config/Images/index.js
@@ -1,6 +1,8 @@
 const Images = {
-    // Logo: require("../../assets/icons/Logo.png"),
-
+    Logo: require("../../assets/images/Logo.png"),
+    SplashBackground: require("../../assets/images/splashbackground.png"),
+    AppBackground: require("../../assets/images/appbackground.png"),
+    MarvelLogo: require("../../assets/images/marvelLogo.png"),
   };
   
   export default Images;
