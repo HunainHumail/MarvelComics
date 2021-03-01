beaf530d82a5aabcd0ad47e79bdc933ecda18ba5
HunainHumail <hunainhumail786@gmail.com>
2021-03-01 12:12:37 +0500

    Added fonts

--- src/config/Fonts/index.js
+++ src/config/Fonts/index.js
@@ -1,11 +1,16 @@
 const Fonts = {
-    // "Poppins-Italic": "Poppins-Italic",
-    // "Poppins-Regular": "Poppins-Regular",
-    // "Poppins-SemiBold": "Poppins-SemiBold",
-    // "Poppins-Bold": "Poppins-Bold",
-    // "Poppins-Bold-Italic": "Poppins-BoldItalic",
-    // "Poppins-Medium": "Poppins-Medium",
-    // "Poppins-Medium-Italic": "Poppins-MediumItalic",
+  "Poppins-Italic": "Poppins-Italic",
+  "Poppins-Regular": "Poppins-Regular",
+  "Poppins-SemiBold": "Poppins-SemiBold",
+  "Poppins-Bold": "Poppins-Bold",
+  "Poppins-Bold-Italic": "Poppins-BoldItalic",
+  "Poppins-Medium": "Poppins-Medium",
+  "Poppins-Medium-Italic": "Poppins-MediumItalic",
+  "Lora-Bold": "Lora-Bold",
+  "Lora-BoldItalic": "Lora-BoldItalic",
+  "Lora-Italic": "Lora-Italic",
+  "Lora-Regular": "Lora-Regular",
+  "Badaboom": "Badaboom"
   };
   
   export default Fonts;
