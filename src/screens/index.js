37b9bb9dbb68c4a899b2f1d2958b224689508321
HunainHumail <hunainhumail786@gmail.com>
2021-03-02 04:55:34 +0500

    Added ChangeCharacterScreen

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,8 +1,10 @@
 import SplashScreen from './Splash'
 import OnboardingScreen from './Onboarding'
 import HomeScreen from './HomeScreen'
+import ChangeCharacterScreen from './ChangeCharacterScreen'
 export {
     SplashScreen,
     OnboardingScreen,
-    HomeScreen
+    HomeScreen,
+    ChangeCharacterScreen
 }
