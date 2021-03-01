0e3e12d8790f40d5d4881952458685bd60fc61c7
HunainHumail <hunainhumail786@gmail.com>
2021-03-02 04:55:01 +0500

    Added ChangeCharacterScreen Stack

--- src/stacks/HomeStack.js
+++ src/stacks/HomeStack.js
@@ -1,9 +1,14 @@
-import {HomeScreen} from '../screens/';
+import {HomeScreen, ChangeCharacterScreen} from '../screens/';
 
 export const HomeStack = [
     {
         name: 'HomeScreen',
         component: HomeScreen,
         key: 'HomeScreen',
+    },
+    {
+        name: 'ChangeCharacterScreen',
+        component: ChangeCharacterScreen,
+        key: 'ChangeCharacterScreen',
     },
 ]
