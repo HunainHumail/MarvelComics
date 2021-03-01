d8782fd48547b5379ddfc906000ce036a9c509e9
HunainHumail <hunainhumail786@gmail.com>
2021-03-02 03:03:08 +0500

    Added KeyboardAvoidingView

--- src/components/AppButton/index.js
+++ src/components/AppButton/index.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import {TouchableOpacity, Text, StyleSheet, ImageBackground} from 'react-native'
+import React,{useState} from 'react'
+import {TouchableOpacity, Text, StyleSheet, ImageBackground, KeyboardAvoidingView} from 'react-native'
 import {Colors, Fonts} from '../../config'
 
-const AppButton = ({width='80%', text, onPress, disabled = true, height}) => {
+const AppButton = ({width='80%', text, onPress, height, disabled}) => {
+    
     return (
         <TouchableOpacity style={[styles.buttonStyle, {width: width, backgroundColor: disabled ? Colors.DisabledColor : Colors.Secondary,height: height}]} onPress={onPress} disabled = {disabled}>
+            <KeyboardAvoidingView>
             <Text style = {styles.textStyle}>{text}</Text>
+            </KeyboardAvoidingView>
         </TouchableOpacity>
     )
 }
