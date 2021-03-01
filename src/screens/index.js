97d1598c03d4672c9789cf104146abf4b608fc9c
HunainHumail <hunainhumail786@gmail.com>
2021-03-02 03:08:31 +0500

    Added HomeScreen

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,6 +1,8 @@
 import SplashScreen from './Splash'
 import OnboardingScreen from './Onboarding'
+import HomeScreen from './HomeScreen'
 export {
     SplashScreen,
     OnboardingScreen,
+    HomeScreen
 }
