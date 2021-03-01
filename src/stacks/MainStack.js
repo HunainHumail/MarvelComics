cf7459642f8f5101a497caf62df51d6e16e774d9
HunainHumail <hunainhumail786@gmail.com>
2021-03-02 03:10:49 +0500

    Added HomeStack to AppStacks

--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {createStackNavigator, TransitionPresets} from '@react-navigation/stack'
 import {InitialStack} from './InitialStack'
+import {HomeStack} from './HomeStack'
+
 export const MainStack = (props) => {
     const MainStack = createStackNavigator();
-    const AppStacks = [...InitialStack];
+    const AppStacks = [...InitialStack, ...HomeStack];
     return(
         <>
         <MainStack.Navigator
