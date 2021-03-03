4b10d996871c87ae5b72334aa8a5b46465a76ffc
Hunain12 <mobapp5.5sd@gmail.com>
2021-03-03 18:51:40 +0500

    removed unused code and comments

--- src/containers/InitialScreens/OnboardingScreen/index.js
+++ src/containers/InitialScreens/OnboardingScreen/index.js
@@ -1,17 +1,14 @@
-import React, { useEffect, useState, useRef } from "react";
+import React from "react";
 import {
   View,
-  Text,
   Image,
   StyleSheet,
   ImageBackground,
-  TextInput,
-  KeyboardAvoidingView,
 } from "react-native";
 import { Fonts, Images, Colors } from "../../../config/";
-import Carousel, { Pagination } from "react-native-snap-carousel";
+import Carousel from "react-native-snap-carousel";
 import {AppButton} from '../../../components'
-const OnboardingScreen = ({sliders, carousel, pagination, onSnapToItem, _renderItem, activeIndex, selectedCharacter, onSelectCb, selectedCharacterId}) => {
+const OnboardingScreen = ({sliders, carousel, pagination, onSnapToItem, _renderItem, activeIndex, onSelectCb, selectedCharacterId}) => {
 
   return (
     <View style={styles.container}>
@@ -55,38 +52,6 @@ const styles = StyleSheet.create({
     flex: 1,
     resizeMode: "cover",
     alignItems: "center",
-    // justifyContent: "center",
-    // width: '100%'
-  },
-  cardStyle: {
-    height: 554,
-    width: 361,
-    backgroundColor: "rgba(255, 255, 255, 0.28)",
-    borderRadius: 15,
-    marginTop: 20,
-  },
-  headingStyle: {
-    fontFamily: Fonts["Badaboom"],
-    fontSize: 45,
-    color: Colors.White,
-    marginHorizontal: "10%",
-    textAlign: "center",
-    marginTop: "2%",
-  },
-  textStyle: {
-    fontFamily: Fonts["Badaboom"],
-    fontSize: 25,
-    color: Colors.White,
-    marginHorizontal: "10%",
-    marginTop: "10%",
-  },
-  textInputStyle: {
-    fontFamily: Fonts["Badaboom"],
-    fontSize: 18,
-    marginHorizontal: '12%',
-    textAlign: 'center',
-    borderBottomColor: Colors.White,
-    borderBottomWidth: 1
   },
   buttonView: {
     width: '30%', justifyContent: 'center', alignItems: 'center'
