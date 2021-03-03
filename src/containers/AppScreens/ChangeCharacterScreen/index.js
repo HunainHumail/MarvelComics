5d8e64604e678d217e2cf874d687c6c7215f5bfd
Hunain12 <mobapp5.5sd@gmail.com>
2021-03-03 18:58:30 +0500

    removed unused code, comments and consoles

--- src/containers/AppScreens/ChangeCharacterScreen/index.js
+++ src/containers/AppScreens/ChangeCharacterScreen/index.js
@@ -1,18 +1,13 @@
-import React, { useEffect, useState, useRef } from "react";
+import React from "react";
 import {
   View,
-  Text,
   Image,
   StyleSheet,
   ImageBackground,
-  TextInput,
-  KeyboardAvoidingView,
-  FlatList,
-  TouchableOpacity,
 } from "react-native";
-import { AppButton, CharacterModal, ComicListTile } from "../../../components";
+import { AppButton } from "../../../components";
 import { Fonts, Images, Colors, Responsive } from "../../../config/";
-const ChangeCharacterScreen = ({ searchComponent, selectedCharacter, onSelectCb, selectedCharacterId }) => {
+const ChangeCharacterScreen = ({ searchComponent, onSelectCb, selectedCharacterId }) => {
 
 
   return (
@@ -41,42 +36,6 @@ const styles = StyleSheet.create({
       flex: 1,
       resizeMode: "cover",
       alignItems: "center",
-      // justifyContent: "center",
-      // width: '100%'
     },
-    cardStyle: {
-      height: 554,
-      width: 361,
-      backgroundColor: "rgba(255, 255, 255, 0.28)",
-      borderRadius: 15,
-      marginTop: 20,
-    },
-    headingStyle: {
-      fontFamily: Fonts["Badaboom"],
-      fontSize: 45,
-      color: Colors.White,
-      marginHorizontal: "10%",
-      textAlign: "center",
-      marginTop: "2%",
-    },
-    textStyle: {
-      fontFamily: Fonts["Badaboom"],
-      fontSize: 25,
-      color: Colors.White,
-      marginHorizontal: "10%",
-      marginTop: "10%",
-    },
-    textInputStyle: {
-      fontFamily: Fonts["Badaboom"],
-      fontSize: 18,
-      marginHorizontal: '12%',
-      textAlign: 'center',
-      borderBottomColor: Colors.White,
-      borderBottomWidth: 1
-    },
-    buttonView: {
-      width: '30%', justifyContent: 'center', alignItems: 'center'
-    }
-  
   });
   
